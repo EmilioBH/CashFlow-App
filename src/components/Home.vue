afb19e56bf386dd6ed5235770a477008ab32854f
Emilio Bonmatí <emilre68@gmail.com>
2022-11-19 12:45:25 +0100

    add movements layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,14 +3,27 @@
         <template #header>
             <Header></Header>
         </template>
+        <template #resume>
+            <Resume />
+        </template>
+        <template #movements>
+            <Movements />
+        </template>
     </Layout>
 </template>
 
 <script>
     import Header from './Header.vue';
     import Layout from './Layout.vue';
+    import Resume from './Resume/Index.vue';
+    import Movements from './Movements.vue';
 
     export default {
-        components: { Header, Layout }
+        components: { 
+            Header, 
+            Layout, 
+            Resume,
+            Movements
+        }
     }
 </script>
