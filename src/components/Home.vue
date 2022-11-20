f2396ed7043beea204951e9c3773aa52bceaba51
Emilio Bonmatí <emilre68@gmail.com>
2022-11-20 20:23:20 +0100

    add form to new amounts

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,10 +4,23 @@
             <Header></Header>
         </template>
         <template #resume>
-            <Resume />
+            <Resume 
+                :label="label" 
+                :totalAmount="1000000"
+                :amount="amount"
+            >
+                <template #graphic>
+                    Graphic
+                </template>
+                <template #action>
+                    <Action />
+                </template>
+            </Resume>
         </template>
         <template #movements>
-            <Movements />
+            <Movements 
+                :movements="movements"
+            />
         </template>
     </Layout>
 </template>
@@ -16,14 +29,54 @@
     import Header from './Header.vue';
     import Layout from './Layout.vue';
     import Resume from './Resume/Index.vue';
-    import Movements from './Movements.vue';
+    import Movements from './Movements/Index.vue';
+    import Action from './Action.vue';
 
     export default {
+        data() {
+            return {
+                amount: null,
+                label: 'Ahorro total',
+                movements: [
+                    {
+                    id: 1,
+                    title: "Movimiento",
+                    description: "Deposito de salario",
+                    amount: "1000",
+                    },
+                    {
+                    id: 2,
+                    title: "Movimiento 1",
+                    description: "Deposito de honorarios",
+                    amount: "500",
+                    },
+                    {
+                    id: 3,
+                    title: "Movimiento 3",
+                    description: "Comida",
+                    amount: "-100",
+                    },
+                    {
+                    id: 4,
+                    title: "Movimiento 4",
+                    description: "Colegiatura",
+                    amount: "1000",
+                    },
+                    {
+                    id: 5,
+                    title: "Movimiento 5",
+                    description: "Reparación equipo",
+                    amount: "1000",
+                    },
+                ],
+            }
+        },
         components: { 
             Header, 
             Layout, 
             Resume,
-            Movements
+            Movements,
+            Action
         }
     }
 </script>
